f98457cd51655889e3f497504d869f731f7b7dac
Malith Kumaranayaka <cobsccomp242p-034@student.nibm.lk>
2026-03-30 14:04:47 +0530

    Initial Commit

---
+++ Elevate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elevate
+//
+//  Created by COBSCCOMP24.2P-034 on 2026-03-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elevate/ElevateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevateApp.swift
+//  Elevate
+//
+//  Created by COBSCCOMP24.2P-034 on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct ElevateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
